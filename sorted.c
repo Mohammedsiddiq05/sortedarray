71fc173aee88e6286990f6ded890e8c8dd787905
Mohammedsiddiq05 <31438748+Mohammedsiddiq05@users.noreply.github.com>
2018-03-07 02:23:39 -0800

    Create sorted.c

---
+++ sorted.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int i,j,a[30],n,p;
+ptrintf("enter the value:);
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+p=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+getch();
+}
+}
